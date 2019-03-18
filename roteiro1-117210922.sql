c3d6a287b309e2d3004471ab08f1b85ca794f025
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-03-18 13:55:58 -0300

    Corrigindo referências.

--- roteiro1-117210922.sql
+++ roteiro1-117210922.sql
@@ -12,7 +12,7 @@ CREATE TABLE AUTOMOVEL(
 CREATE TABLE SEGURADO(
 	nome VARCHAR(20),
 	cpf VARCHAR(13),
-	numero_seguro VARCHAR(10),
+	numero_seguro SERIAL,
 	valor_seguro NUMERIC
 );
 
@@ -29,7 +29,7 @@ CREATE TABLE OFICINA(
 CREATE TABLE SEGURO(
 	numero_seguro SERIAL,
 	valor_seguro NUMERIC,
-	placa_veiculo VARCHAR(8)
+	noFab VARCHAR(17)
 );
 
 CREATE TABLE SINISTRO(
@@ -40,13 +40,16 @@ CREATE TABLE SINISTRO(
 );
 
 CREATE TABLE PERICIA(
-	nome VARCHAR(20),
-	cpf VARCHAR(13)
+	id_pericia SERIAL,
+	cpf_perito VARCHAR(13),
+	sinistro VARCHAR(10)
 );
 
 CREATE TABLE REPARO(
-	placa_veiculo VARCHAR(20),
-	custo_reparo VARCHAR(20)
+	id_reparo SERIAL,
+	noFab VARCHAR(17),
+	custo_reparo VARCHAR(20),
+	cnpj_oficina VARCHAR(20)
 );
 
 -- Questão 3
@@ -63,9 +66,9 @@ ALTER TABLE seguro ADD PRIMARY KEY (numero_seguro);
 
 ALTER TABLE sinistro ADD PRIMARY KEY (placa_veiculo, hora);
 
-ALTER TABLE pericia ADD PRIMARY KEY (cpf);
+ALTER TABLE pericia ADD PRIMARY KEY (id_pericia);
 
-ALTER TABLE reparo ADD PRIMARY KEY (placa_veiculo);
+ALTER TABLE reparo ADD PRIMARY KEY (id_reparo);
 
 
 -- Questão 4
@@ -73,6 +76,17 @@ ALTER TABLE reparo ADD PRIMARY KEY (placa_veiculo);
 ALTER TABLE automovel ADD CONSTRAINT automovel_cpfProprietario FOREIGN KEY (cpf_proprietario) REFERENCES segurado(cpf);
 
 ALTER TABLE segurado ADD CONSTRAINT segurado_noSeguro FOREIGN KEY(numero_seguro) REFERENCES seguro(numero_seguro);
+
+ALTER TABLE seguro ADD CONSTRAINT seguro_placa_veiculo FOREIGN KEY(noFab) REFERENCES automovel(noFab);
+
+ALTER TABLE sinistro ADD CONSTRAINT sinistro_placa_veiculo FOREIGN KEY(placa_veiculo) REFERENCES automovel(placa);
+
+ALTER TABLE pericia ADD CONSTRAINT pericia_cpf_perito FOREIGN KEY(cpf_perito) REFERENCES perito(cpf);
+
+ALTER TABLE reparo ADD CONSTRAINT reparo_placa_veiculo FOREIGN KEY(nofab) REFERENCES automovel(noFab);
+
+ALTER TABLE reparo ADD CONSTRAINT reparo_cnpj_oficina FOREIGN KEY(cnpj_oficina) REFERENCES oficina(cnpj);
+
 
 
 
